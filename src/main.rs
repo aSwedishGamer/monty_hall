f35aa93ba5ae6641dc8e4acda14bc9c59be75646
aSwedishGamer <david.carbeskar@gmail.com>
2024-03-13 15:26:34 +0100

    Rewrote everything

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,27 @@
 use rand::Rng;
-use std::{array, io::stdin};
+use std::io::stdin;
 fn main() {
-    let winner: usize = rand::thread_rng().gen_range(0..=3);
+    //take input
     println!("Please type a number from 1 to 3:");
     let mut input = String::new();
     let _ = stdin().read_line(&mut input);
-    let input: i8 = input.trim().parse().unwrap();
-    let mut array: [i8; 3] = [0; 3];
-    if input == winner.try_into().unwrap() {
-        array[winner - 1] = 2;
-    } else {
-        array[winner - 1] = 1;
+    let input: usize = input.trim().parse().unwrap();
+    let mut selected_door = [false; 3];
+    selected_door[input - 1] = true;
+
+    //selecting winning door
+    let winner: usize = rand::thread_rng().gen_range(0..=2);
+    let mut winning_door = [false; 3];
+    winning_door[winner] = true;
+
+    //remove a door that is niether winning or selected
+    loop {
+        let random = rand::thread_rng().gen_range(0..=2);
+        println!("{random}");
+        if winning_door[random] && selected_door[random - 1] == false {
+            let removed_door = random;
+            println!("{}", removed_door);
+            break;
+        }
     }
 }
